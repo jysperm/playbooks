8c7a31a95ae5831ec6f0cf4e4de836e8303f0ed5
jysperm <jysperm@gmail.com>
2017-11-12 15:00:06 +0800

    :truck: Rename leanengine-proxy to lc-proxy

--- lc-proxy/server.js
+++ lc-proxy/server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(host('ziting.wang', express.static('public/ziting.wang')));
 app.use(host('jysperm.me', express.static('public/jysperm.me')));
 app.use(host('caipai.fm', express.static('public/caipai.fm')));
-app.use(host('jybox.net', express.static('public/jybox.net')));
-app.use(host('atom-china.org', proxy('140.143.189.132:49151')));
+app.use(host('atom-china.org', proxy('140.143.189.132:8443')));
 
 app.get('/', (req, res, next) => {
   res.json({ping: 'pong'});
